41782eb6994ccfdd01171924ae5f6f55f4d9c919
Patje2001 <97318792+Patje2001@users.noreply.github.com>
2022-01-09 12:08:39 +0100

    Add files via upload

---
+++ db/migrate/20211213124206_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.references :customer, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
